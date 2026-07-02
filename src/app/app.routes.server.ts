3907ffd2ad60f322825628fd7ef9f5dae31fb8ad
Jinjerana <Jinjerana@gmail.com>
2026-07-02 10:31:57 +0200

    fix: switch note/:id route to client-side rendering

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'note/:id',
+    renderMode: RenderMode.Client
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
